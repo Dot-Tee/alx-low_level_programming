d075b4b38e1a0353a68395b9618eb6463de2d074
Taiwo <taiwo1695@gmail.com>
2022-09-08 13:48:42 -0700

    first c coding

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "with proper grammer, but the outcome is a piece of act,"
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n);
+	return (0);
+}
