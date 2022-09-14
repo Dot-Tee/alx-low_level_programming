973f5b2c1b7d03469180d6f912785bfd742f24b2
Taiwo <taiwo1695@gmail.com>
2022-09-14 13:30:45 -0700

    c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,12 +12,11 @@
 void print_remaining_days(int month, int day, int year)
 {
 if ((year % 400 == 0) || (year % 4 == 0 && year % 100 !=  0))
-
 {
 if (month >= 3 && day >= 60)
 {
 day++;
-iii}
+}
 
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
@@ -35,4 +34,3 @@ printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
-
