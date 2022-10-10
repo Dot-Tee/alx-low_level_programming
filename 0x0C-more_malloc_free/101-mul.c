9888ba2d59807264e99136a65cc99eeaecb01762
Taiwo <taiwo1695@gmail.com>
2022-10-10 12:56:46 -0700

    commit on marco and prepocessor use

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,8 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 
-
-
 /**
  * _print - moves a string one place to the left and prints the string
  * @str: string to move
@@ -28,8 +26,6 @@ void _print(char *str, int l)
 	_putchar('\n');
 	free(str);
 }
-
-
 
 /**
  * mul - multiplies a char with a string and places the answer into dest
@@ -93,7 +89,6 @@ char *mul(char n, char *num, int num_index, char *dest, int dest_index)
  */
 
 int check_for_digits(char **av)
-
 {
 
 	int i, j;
@@ -120,8 +115,6 @@ int check_for_digits(char **av)
 
 }
 
-
-
 /**
  * init - initializes a string
  * @str: sting to initialize
@@ -146,8 +139,6 @@ void init(char *str, int l)
 
 }
 
-
-
 /**
  * main - multiply two numbers
  * @argc: number of arguments
@@ -157,70 +148,64 @@ void init(char *str, int l)
  */
 
 int main(int argc, char *argv[])
-
 {
-
 	int l1, l2, ln, ti, i;
-
 	char *a;
-
 	char *t;
-
 	char e[] = "Error\n";
 
-
-
 	if (argc != 3 || check_for_digits(argv))
-
 	{
-
 		for (ti = 0; e[ti]; ti++)
-
 			_putchar(e[ti]);
-
 		exit(98);
-
 	}
 
 	for (l1 = 0; argv[1][l1]; l1++)
-
 		;
 
 	for (l2 = 0; argv[2][l2]; l2++)
-
 		;
-
 	ln = l1 + l2 + 1;
-
 	a = malloc(ln * sizeof(char));
 
 	if (a == NULL)
-
 	{
-
-		for (ti = 0; e[ti]; ti++)
-
-			_putchar(e[ti]);
-
-		exit(98);
-
+	for (ti = 0; e[ti]; ti++)
+		_putchar(e[ti]);
+	exit(98);
 	}
 
 	init(a, ln - 1);
 
+
 	for (ti = l2 - 1, i = 0;
-			 ti >= 0; ti--, i++)
+
+			ti >= 0; ti--, i++)
+
 	{
-		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
+
+		t = mul(argv[2][ti], argv[1],
+
+				l1 - 1, a, (ln - 2) - i);
 
 		if (t == NULL)
+
 		{
-			for (ti = 0; e[ti]; ti++)
+
+			for (t1 = 0; e[ti]; ti++)
 				_putchar(e[ti]);
+
 			free(a);
+
 			exit(98);
+
 		}
+
 	}
+
 	_print(a, ln - 1);
+
 	return (0);
+
 }
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _SUM_LIKE_MARCO_
+#define _SUM_LIKE_MARCO_
+
+#define SUM(X, y) ((x) + (y))
+
+#endif /* _SUM_LIKE_MARCO_ */
