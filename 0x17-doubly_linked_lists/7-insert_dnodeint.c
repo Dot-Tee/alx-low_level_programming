6c78dcfe2a6fd70a50c412033f092dc65935e704
Taiwo <taiwo1695@gmail.com>
2022-12-29 19:03:55 -0800

    correction

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,6 +3,7 @@
 /**
  * insert_dnodeint_at_index - inserts a new node at
  * a given position
+ *
  * @h: head of the list
  * @idx: index of the new node
  * @n: value of the new node
@@ -14,9 +15,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new;
 	dlistint_t *head;
 	unsigned int i;
-	
-	new = NULL;
 
+	new = NULL;
 	if (idx == 0)
 		new = add_dnodeint(h, n);
 	else
@@ -34,22 +34,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 					new = add_dnodeint_end(h, n);
 				else
 				{
-					new = malloc(sizeof(dlistint_t);
-	if (new != NULL)
-	{
-		new->n = n;
-		new->next = head->next;
-		new->prev = head;
-		head->next->prev = new;
-		head->next = new;
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
 	}
-} 
-                                 break; 
-                         } 
-                         head = head->next; 
-                         i++; 
-                 } 
-         } 
-  
-         return (new); 
- }
+
+	return (new);
+}
